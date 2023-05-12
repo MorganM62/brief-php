17120e1d25f618d67ade2fd69abccd16d2ab4f45
Supramout <moutonmorgan@orange.fr>
2023-05-12 15:19:54 +0200

    début du projet

---
+++ php/index.php
@@ -0,0 +1,19 @@
+session_start()
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php 
+       include "./includes/head.inc.html"; 
+       include "./includes/header.inc.html";
+    ?>
+    
+    <title>Formulaire Php</title>
+</head>
+<body>
+    
+</body>
+</html>
