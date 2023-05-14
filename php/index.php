2efb407e4cdf5a04220d058352cded272d3ba7d9
Supramout <moutonmorgan@orange.fr>
2023-05-14 12:01:36 +0200

    add nav barre

--- php/index.php
+++ php/index.php
@@ -7,13 +7,29 @@ session_start()
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php 
-       include "./includes/head.inc.html"; 
-       include "./includes/header.inc.html";
+       include "./includes/head.inc.html";
     ?>
     
     <title>Formulaire Php</title>
 </head>
 <body>
-    
+   <?php 
+        include "./includes/header.inc.html";
+    ?>
+    <div class="card border-primary ms-4 mt-4 text-center" style="max-width: 11rem;">
+        <div class="card-header">Home</div>
+    </div>
+    <div class="list-group ms-4  text-center" style="max-width : 11rem;">
+        <a href="#" class="list-group-item list-group-item-action active">Débogage</a>
+        <a href="#" class="list-group-item list-group-item-action">Concaténation</a>
+        <a href="#" class="list-group-item list-group-item-action">Boucle</a>
+        <a href="#" class="list-group-item list-group-item-action">Fonction</a>
+        <a href="#" class="list-group-item list-group-item-action">Supprimer</a>
+    </div>
+    <button type="button" class="btn btn-primary ms-4 mt-4 inline">Ajouter des données</button>
+    <?php 
+     include "./includes/footer.inc.html";
+    ?>
+
 </body>
 </html>
