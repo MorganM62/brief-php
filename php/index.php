f9f154ee401bb3f4f7b9278a0f2753590009148a
Supramout <moutonmorgan@orange.fr>
2023-05-22 16:21:02 +0200

    index.php > add button (not ok)

--- php/index.php
+++ php/index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$table = $_SESSION["table"];
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -19,22 +18,48 @@ $table = $_SESSION["table"];
         include "./includes/header.inc.html";
         ?>
     
+    <div class="d-flex justify-content-start">
+                <div class="ms-5 mt-3">
+            <a href="index.php">
+                <button type="button" class="btn btn-outline-secondary" style="min-width: 175px; max-height:39px;">Home</button>
+            </a>
+        </div>
+    </div>
     <?php
         if (isset($_GET['add'])){
             include "./includes/form.inc.html";
         }
+        elseif (isset($_POST['register'])){
+            $prénom = $_POST['first_name'];
+            $nom = $_POST['last_name'];
+            $age = $_POST['age'];
+            $size = $_POST['size'];
+            $gender = $_POST['gender'];
+
+            $table = array(
+                'first_name' => $prénom,
+                'last_name' => $nom,
+                'age' => $age,
+                'size' => $size,
+                'gender' => $gender,
+            );
+
+            $_SESSION['table'] = $table;
+            echo '<div class="alert alert-success" role="alert">
+            Données  !
+          </div>';
+        }
         else {
             echo '
-            <div class="d-flex justify-content-start">
-                <div class="ms-5 mt-3">
-                    <a href="index.php">
-                        <button type="button" class="btn btn-outline-secondary" style="min-width: 175px; max-height:39px;">Home</button>
-                    </a>
-                </div>
-                <div class="ms-4 mt-3">
-                    <a href="index.php?add">
-                        <button type="submit" class="btn btn-primary">Ajouter des données</button>
-                    </a>
+            <div class="container overflow-hidden">
+                <div class="row gx-4">
+                    <div class ="inline">
+                        <div class ="p3">
+                            <a href="index.php?add">
+                                <button type="submit" class="btn btn-primary">Ajouter des données</button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>';
         }
