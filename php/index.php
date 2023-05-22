688f1745450fdbef918de6dd54da18fc562c0100
Supramout <moutonmorgan@orange.fr>
2023-05-22 15:04:10 +0200

    add form

--- php/index.php
+++ php/index.php
@@ -1,5 +1,6 @@
-session_start()
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,21 +16,31 @@ session_start()
 <body>
    <?php 
         include "./includes/header.inc.html";
+        ?>
+    <div class="d-flex justify-content-start">
+        <div class="ms-5 mt-3">
+            <a href="index.html">
+            <button type="button" class="btn btn-outline-secondary" style="min-width: 175px; max-height:39px;">Home</button>
+            </a>
+         </div>
+        <div class="ms-4 mt-3">
+            <a href="index.php?add">
+                <button type="submit" class="btn btn-primary">Ajouter des données</button>
+            </a>
+        </div>
+    </div>
+    
+    <?php
+        if (isset($_GET['add'])){
+            
+            include "./includes/form.inc.html";
+        }
     ?>
-    <div class="card border-primary ms-4 mt-4 text-center" style="max-width: 11rem;">
-        <div class="card-header">Home</div>
-    </div>
-    <div class="list-group ms-4  text-center" style="max-width : 11rem;">
-        <a href="#" class="list-group-item list-group-item-action active">Débogage</a>
-        <a href="#" class="list-group-item list-group-item-action">Concaténation</a>
-        <a href="#" class="list-group-item list-group-item-action">Boucle</a>
-        <a href="#" class="list-group-item list-group-item-action">Fonction</a>
-        <a href="#" class="list-group-item list-group-item-action">Supprimer</a>
-    </div>
-    <button type="button" class="btn btn-primary ms-4 mt-4 inline">Ajouter des données</button>
-    <?php 
+    
+    <?php
      include "./includes/footer.inc.html";
     ?>
+
 
 </body>
 </html>
